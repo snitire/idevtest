503804fc4909a978631f6fbff665650a5c9ce642
snitire <elvis.ritins@gmail.com>
2025-11-30 22:34:46 +0200

    add functionality to start and end tasks

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const BASE_URL = "http://10.101.0.1:10101/"
 const TOKEN_URL = BASE_URL + "?token=renew"
-const DATA_URL = BASE_URL + "task"
+const TASK_URL = BASE_URL + "task"
+const CSV_URL = BASE_URL + "csv"
 
 // ideally this sort of stuff goes in environment variables not the code
 const tokenAuthFile = "./userpass.txt"
@@ -18,6 +19,12 @@ async function loadTokenAuth() {
 }
 await loadTokenAuth()
 
+const TOKEN_BLANK = {
+    value: "",
+    created: 0,
+    expires: 0
+}
+
 async function fetchToken() {
     try {
         // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
@@ -33,22 +40,69 @@ async function fetchToken() {
         // https://developer.mozilla.org/en-US/docs/Web/XML/Guides/Parsing_and_serializing_XML
         const tokenXml = new DOMParser().parseFromString(await response.text(), "application/xml")
 
-        let result = {
-            value: "",
-            created: "",
-            expires: ""
-        }
+        let result = TOKEN_BLANK
 
         if (tokenXml.querySelector("errorNode")) {
             throw new Error("Cant parse token: " + tokenXml.querySelector("errorNode"))
         } else {
             result.value = tokenXml.querySelector("Value").textContent
-            result.created = Date.parse(tokenXml.querySelector("Created").textContent)
-            result.expires = Date.parse(tokenXml.querySelector("Expires").textContent)
+            // force parse to interpret the given date as UTC, a little hacky but it works
+            // https://stackoverflow.com/questions/32252565/javascript-parse-utc-date
+            result.created = Date.parse(tokenXml.querySelector("Created").textContent + "Z")
+            result.expires = Date.parse(tokenXml.querySelector("Expires").textContent + "Z")
+            console.log("Successfully fetched token: ")
+            console.log(result)
             return result
         }
     } catch (e) {
         console.error(e.message)
-        return null
+        return TOKEN_BLANK
     }
 }
+
+let token = TOKEN_BLANK
+
+// https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function postToTask(body) {
+    // renews token a minute early just to be sure since there is no real renewal limit
+    // this might spam the server with requests if the fetch fails so some sort of timeout is nice
+    while ((Date.now() + 1000*60) >= token.expires) {
+        token = await fetchToken()
+        await sleep(1000)
+    }
+
+    return fetch(TASK_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": "Bearer " + token.value
+        },
+        body: body
+    })
+}
+
+async function startTask() {
+    const response = await postToTask("command=begin")
+
+    if (!response.ok) {
+        console.error("Failed to start task: " + response.status)
+    } else {
+        console.log("Successfully started task: " + response.status)
+    }
+}
+
+async function endTask() {
+    const response = await postToTask("command=end")
+
+    if (!response.ok) {
+        console.error("Failed to end task: " + response.status)
+    } else {
+        console.log("Successfully ended task: " + response.status)
+    }
+}
+
+await startTask()
+await endTask()
